d7c5efcb9f4faec15c2827e7329bd238e46a7751
doyougnu <jmy6342@gmail.com>
2019-09-16 15:24:21 -0700

    helper function for making executable string

--- SBVBenchSuite/Utils/SBVBenchFramework.hs
+++ SBVBenchSuite/Utils/SBVBenchFramework.hs
@@ -2,6 +2,7 @@
 -- |
 -- Module    : Utils.SBVTestFramework
 -- Copyright : (c) Jeffrey Young
+--                 Levent Erkok
 -- License   : BSD3
 -- Maintainer: erkokl@gmail.com
 -- Stability : experimental
@@ -9,5 +10,21 @@
 -- Various goodies for benchmarking SBV
 -----------------------------------------------------------------------------
 
-
 module Utils.SBVBenchFramework where
+
+import qualified Data.List      as L
+import           System.Process (showCommandForUser)
+
+import qualified Data.SBV       as S
+
+-- | make the string to call executable from the command line. All the heavy
+-- lifting is done by 'System.Process.showCommandForUser', the rest is just
+-- projections out of 'Data.SBV.SMTConfig'
+mkExecString :: S.SMTConfig -> FilePath -> String
+mkExecString config inputFile = showCommandForUser exec $ inputFile:opts
+  where smtSolver = S.solver config
+        exec      = S.executable smtSolver
+        opts'     = S.options smtSolver config
+        opts      = L.delete "-in" opts' -- remove opt for interactive mode so
+                                         -- that this plays nice with
+                                         -- criterion environments
